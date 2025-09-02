7b76a95b1bd0c84a33e4218a576b5e0de15cef47
Pedro Reis <126162932+pesantos12@users.noreply.github.com>
2025-09-02 10:40:45 -0300

    Add files via upload

--- AtividadeTree.py
+++ AtividadeTree.py
@@ -1,3 +1,5 @@
+from treelib import Tree
+from collections import deque
 
 class Node:
     def __init__(self, data):
@@ -7,7 +9,32 @@ class Node:
 
 class BinaryTree:
     def __init__(self):
+        self.tree = Tree()
+        self.fila = deque()
         self.root = None
+
+
+    def addNode(self):
+        i = None
+        while(i != ""):
+            i = input("\nDigite o valor do nó a ser inserido (Deixe vazio para encerrar):")
+            if i != "":
+                if len(self.tree) == 0:
+                    self.tree.create_node(i, i)
+                    self.fila.append(i)
+                    self.fila.append(i)
+                else:
+                    self.fila.append(i)
+                    self.fila.append(i)
+                    self.tree.create_node(i, i, parent=self.fila.popleft())
+            else:
+                break
+
+
+
+
+
+
 
     def insert_level_order(self, data): #inserção automática por nível
         # implementar
@@ -49,6 +76,27 @@ class BinaryTree:
         # Retorna true se não for balanceada
         pass
 
+    def mostrarArvore(self):
+        print('\n')
+        self.tree.show()
 
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
+    arvore = BinaryTree()
+    arvore.addNode()
+    arvore.mostrarArvore()
+
+
+
+
+"""
+    tree.create_node("n1", "n1", data=20)
+
+    tree.create_node("n2", "n2", parent="n1", data=10)
+    tree.create_node("n3", "n3", parent="n1", data=30)
+    tree.create_node("n4", "n4", parent="n2", data=40)
+    print(f"Tree size: {tree.size()}")           # Number of nodes
+    print(f"Tree depth: {tree.depth()}")         # Maximum depth
+    print(f"Root node: {tree.root}")             # Root identifier
+    print(f"Is empty: {len(tree) == 0}")         # Empty check"""
