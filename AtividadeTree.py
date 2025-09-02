d75d05420bf7bd547ce8d507e76a302da6e5b7ff
Pedro Reis <126162932+pesantos12@users.noreply.github.com>
2025-09-02 09:55:37 -0300

    Add files via upload

---
+++ AtividadeTree.py
@@ -0,0 +1,54 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert_level_order(self, data): #inserção automática por nível
+        # implementar
+        pass
+
+    def inorder(self, node):
+        # implementar
+        pass
+
+    def preorder(self, node):
+        # implementar
+        pass
+
+    def postorder(self, node):
+        # implementar
+        pass
+
+    def level_order(self):
+        # implementar
+        pass
+
+    def is_perfect(self):
+        # Retorna true se a árvore for perfeita
+        pass
+
+    def is_complete(self):
+        # Retorna true se a árvore for completa
+        pass
+
+    def is_regular(self):
+        # Retorna true se todos os nós tiverem 0 ou 2 filhos
+        pass
+
+    def is_balanced(self):
+        # Retorna true se for balanceada
+        pass
+
+    def is_unbalanced(self):
+        # Retorna true se não for balanceada
+        pass
+
+
+
+if __name__ = '__main__':
